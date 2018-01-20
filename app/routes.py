6116cc152586d0155d3c19a4b551babff1c34ab1
nishijayaraj <nishi@debian>
2018-01-20 23:52:13 +0530

    Json rendered

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from itertools import islice
 from flask import render_template
 from app import app
+import requests
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello World"
+  title = "sample app"
+  url = "http://localhost:9000"
+  #res = requests.get(url)
+  #data=res.json()
+  data = [ {
+    "Timestamp": 0,
+    "Voltage": 0,
+    "Current": 0,
+    "Resistance": 0}]
+    
+  return render_template("index.html",title=title, data=data[0]) 
