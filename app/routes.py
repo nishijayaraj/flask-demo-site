7ffc31e34e2a344c3bae43035407f2510f597b2b
nishijayaraj <nishi@debian>
2018-01-21 18:41:07 +0530

    rest api and narrowed to 10 items

--- app/routes.py
+++ app/routes.py
@@ -8,13 +8,17 @@ import requests
 @app.route('/index')
 def index():
   title = "sample app"
-  url = "http://localhost:9000"
-  #res = requests.get(url)
-  #data=res.json()
-  data = [ {
+  url = "http://www.mocky.io/v2/5a6485cf2b00000509f41487"
+  res = requests.get(url)
+  print('*************')
+  print res
+  data=res.json()
+  print data
+  display_items = ["Timestamp","Voltage","Current", "Resistance", "Battery", "Vehicle speed","Diagnostic message","Engine speed", "Engine oil level", "Coolant level"]
+  '''data = [ {
     "Timestamp": 0,
     "Voltage": 0,
     "Current": 0,
-    "Resistance": 0}]
+    "Resistance": 0}]'''
     
-  return render_template("index.html",title=title, data=data[0]) 
+  return render_template("index.html",title=title, data=data[0], display_items = display_items) 
