7a790ae434aa8d61deb6c2cc8a6f14aeb1470923
nishijayaraj <nishi@debian>
2018-01-22 01:54:06 +0530

    V1.0 : stable version

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,34 @@
 from itertools import islice
 from flask import render_template
+from flask import request
 from app import app
 import requests
-
+ 
+values =[]
+title = "sample app"
 
 @app.route('/')
 @app.route('/index')
 def index():
-  title = "sample app"
-  url = " http://www.mocky.io/v2/5a64930e2b0000a20af41495"
-  res = requests.get(url)
-  print('*************')
-  print res
-  data=res.json()
-  print data
-  display_items = ["Timestamp","Voltage","Current", "Resistance", "Battery", "Vehicle speed","Diagnostic message","Engine speed", "Engine oil level", "Coolant level"]
-  '''data = [ {
-    "Timestamp": 0,
-    "Voltage": 0,
-    "Current": 0,
-    "Resistance": 0}]'''
-    
-  return render_template("index.html",title=title, data=data, display_items = display_items) 
+  if not values:
+     return "Please send data to this application..."
+  else:
+     display_items = ["Timestamp","Voltage","Current", "Resistance", "Battery", "Vehicle speed","Diagnostic message","Engine speed", "Engine oil","Coolant level"] 
+     return render_template("index.html", data=values, display_items = display_items)
+  
+  
+
+@app.route('/data', methods=['POST'])
+def push_data(): 
+    values.append(request.json[0])
+    return "success.."
+
+@app.route('/data', methods=['GET'])
+def fetch_data(): 
+    if not values:
+       return "Please send data to this application..."
+    else:
+       display_items = ["Timestamp","Voltage","Current", "Resistance", "Battery", "Vehicle speed","Diagnostic message","Engine speed", "Engine oil","Coolant level"]     
+       return render_template("index.html",title=title, data=values, display_items = display_items)
+
+
